6497edbc3a71d82494bc23e2dcdafc038b7447bd
Travis Cooper <dozertdc@gmail.com>
2017-07-23 21:13:43 -1100

    version 3 finished

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-//It should have a place to store todos
-var todos = ['item 1', 'item 2', 'item 3'];
+//It should store the todos array on an object
+//It should have a display todos method
+//It should have an addTodo method
+// It should have a changeTodo method
+//It should have a deleteTodo method
 
-//It should have a function to display todos
-function displayTodos(){
-    console.log("My Todos: " + todos);
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function(){
+        console.log("My Todos: " + this.todos);
+    },
+    addTodo: function(todo){
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue){
+        this.todos[position]=newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
 }
 
-//It should have a function to add todos
-function addTodo(todo){
-    todos.push(todo);
-    displayTodos();
-}
-// It should have a function to change todos
-function changeTodo(position, newValue){
-    todos[position]=newValue;
-    displayTodos();
-}
-
-
-//It should have a function to delete todos
-function deleteTodo(position){
-    todos.splice(position, 1);
-    displayTodos();
-}
 
 //testing
-displayTodos();
-addTodo('added todo');
-changeTodo(1, 'item 2 updated');
-deleteTodo(0,1);
+todoList.displayTodos();
+todoList.addTodo('added todo');
+todoList.changeTodo(1, 'item 2 updated');
+todoList.deleteTodo(0,1);
 
