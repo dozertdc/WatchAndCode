a661a2df1d874d4f99ef3302f62402a911f03cea
Travis Cooper <dozertdc@gmail.com>
2017-07-23 20:47:48 -1100

    version 2 complete

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 //It should have a place to store todos
 var todos = ['item 1', 'item 2', 'item 3'];
 
-//It should have a way to display todos
-console.log('My Todos: ' + todos);
+//It should have a function to display todos
+function displayTodos(){
+    console.log("My Todos: " + todos);
+}
 
-//It should have a way to add todos
-todos.push('item 4');
+//It should have a function to add todos
+function addTodo(todo){
+    todos.push(todo);
+    displayTodos();
+}
+// It should have a function to change todos
+function changeTodo(position, newValue){
+    todos[position]=newValue;
+    displayTodos();
+}
 
-// It should have a way to change todos
-todos[0]='item 1 updated';
 
-//It should have a way to delete todos
-todos.splice(0, 1);
+//It should have a function to delete todos
+function deleteTodo(position){
+    todos.splice(position, 1);
+    displayTodos();
+}
 
-console.log(todos);
+//testing
+displayTodos();
+addTodo('added todo');
+changeTodo(1, 'item 2 updated');
+deleteTodo(0,1);
 
