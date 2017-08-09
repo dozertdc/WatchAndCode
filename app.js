b7fe6943104a53d65e8498f5992aa3f7d17ba0f7
Travis Cooper <dozertdc@gmail.com>
2017-08-08 21:25:37 -1100

    finished version 8

--- app.js
+++ app.js
@@ -67,14 +67,36 @@ var todoList = {
 // We want to get access to the display todos button
 // We want to run the displayTodos method, when clicks the display
 // todos button.
-
-var displayTodosButton = document.getElementById('displayTodosButton');
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();
-});
-
-//
-var toggleAllButton = document.getElementById('toggleAllButton');
-toggleAllButton.addEventListener('click', function(){
-    todoList.toggleAll();
-});
+var handlers = {
+    displayTodos: function(){
+        todoList.displayTodos();
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+    },
+    addTodo: function(){
+        var addTodoTextInput = document.getElementById('addTodoTextInput');
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = "";
+    },
+    changeTodo: function(){
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+    },
+    deleteTodo: function(){
+        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        changeTodoPositionInput.value = "";
+    },
+    toggleCompleted: function(){
+        var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = "";
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+    }
+};
